f4951e4bc9defba6067ddb94f1535f043f57d2b2
Sanashahid <sanashd20@gmail.com>
2024-02-02 11:51:27 +0500

    errors removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,10 +133,26 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+  // on('click', '.scrollto', function(e) {
+  //   if (select(this.hash)) {
+  //     e.preventDefault()
 
+  //     let navbar = select('#navbar')
+  //     if (navbar.classList.contains('navbar-mobile')) {
+  //       navbar.classList.remove('navbar-mobile')
+  //       let navbarToggle = select('.mobile-nav-toggle')
+  //       navbarToggle.classList.toggle('bi-list')
+  //       navbarToggle.classList.toggle('bi-x')
+  //     }
+  //     scrollto(this.hash)
+  //   }
+  // }, true)
+
+
+  on('click', '.scrollto', function(e) {
+    if (this.hash && select(this.hash)) {
+      e.preventDefault()
+  
       let navbar = select('#navbar')
       if (navbar.classList.contains('navbar-mobile')) {
         navbar.classList.remove('navbar-mobile')
