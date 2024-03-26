aac2c490bd2ba87d2edf07de0f31fd0ae7edee96
ZainabN96 <zainabnaveed2512@gmail.com>
2024-03-26 16:57:53 +0500

    Add phone number

--- contact_us.php
+++ contact_us.php
@@ -41,103 +41,129 @@
 <?php
 
 
-    //$to = "hafizumerkhan1@gmail.com";
-    //$subject = "Test Email";
-    //$message = "This is a test email.";
-    //$headers = "From: khannnn839@gmail.com";
+//     //$to = "hafizumerkhan1@gmail.com";
+//     //$subject = "Test Email";
+//     //$message = "This is a test email.";
+//     //$headers = "From: khannnn839@gmail.com";
+//     $to = "zainabnaveed.hcc@gmail.com";
+//     $name = $_POST["name"];
+//     $email = $_POST["email"];
+//     $subject = $_POST["subject"];
+//     $message = $_POST["message"];
+//     $headers = "From: " . $name . " <" . $email . ">\r\n";
+//     $headers .= "Reply-To: " . $email . "\r\n"; // Add a Reply-To header
+
+//     // Send email
+//     if (mail($to, $subject, $message, $headers)) {
+//         echo "OK";
+//     } else {
+//         echo "Email sending failed.";
+//         // Additional error handling
+//         //echo "Error: " . error_get_last()['message'];
+//     }
+
+
+// exit;
+
+//     /*$to = "ayeshasheikh.as23@gmail.com";
+//     $name = $_POST["name"];
+//     $email = $_POST["email"];
+//     $subject = $_POST["subject"];
+//     $message = $_POST["message"];
+    
+//     // Set additional headers
+//     $headers = "From: $email\r\n";
+//     $headers .= "Reply-To: $email\r\n";
+//     $headers .= "MIME-Version: 1.0\r\n";
+//     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+
+//     // Send email
+//     if (mail($to, $subject, $message, $headers)) {
+//         echo "Email sent successfully.";
+//     } else {
+//         echo "Email sending failed.";
+//         // Additional error handling
+//         //echo "Error: " . error_get_last()['message'];
+//     }
+// exit;*/
+//     use PHPMailer\PHPMailer\PHPMailer;
+//     use PHPMailer\PHPMailer\SMTP;
+//     use PHPMailer\PHPMailer\Exception;
+
+//     require 'vendor/autoload.php';
+
+//   //  $toEmail = "Technical.trainer@hcctechfoundation.com";
+//       $toEmail = "khannnn839@gmail.com";
+//     // $name = $_POST["name"];
+//     // $email = $_POST["email"];
+//     // $subject = $_POST["subject"];
+//     // $message = $_POST["message"];
+//     $name = 'HAMZA';
+//     $email = "hafizumerkhan1@gmail.com";
+//     $subject = 'Test';
+//     $message ="Test email";
+//     $mail = new PHPMailer(true);
+
+//     try {
+//         // $mail->isSMTP();
+//         // $mail->Host = 'smtp.gmail.com';
+//         // $mail->SMTPSecure = 'tls';
+//         // $mail->Port = 587;
+//         // $mail->SMTPAuth = true;
+//         // $mail->Username = 'zainabnaveed.hcc@gmail.com';
+//         // $mail->Password = 'Zain@2512';
+//          $mail->isSMTP();
+//         // $mail->Host       = 'smtp.meshmavens.com';
+//         // $mail->Host       = 'localhost';
+//         $mail->Host       = 'meshmavens.com';
+//         $mail->SMTPSecure = 'tls'; 
+//         $mail->SMTPAuth   = true;
+//         //$mail->SMTPAuth   = false;
+//        // $mail->Username   = 'info@meshmavens.com';
+//        // $mail->Password   = 'Temp*4143';
+//         $mail->Username   = 'khannnn839@gmail.com';
+//         $mail->Password   = 'bywm mshj rran qhma';
+//         $mail->Port = 587;
+
+//         $mail->setFrom($email, $name);
+//         $mail->addAddress($toEmail, 'Mesh team');
+
+//         $mail->isHTML(true);
+//         $mail->Subject = $subject;
+//         $mail->Body = $message;
+
+//        $res =  $mail->send();
+//        if($mail->send()){
+//               echo "Your message has been sent. Thank you!";
+//        }
+//        echo $res;
+//     } catch (Exception $e) {
+//         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+//     }
+?>
+<?php
+
     $to = "zainabnaveed.hcc@gmail.com";
+    //$to = "contact@mansoorsarwar.com";
+    //$to = "info@meshmavens.com";
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $number = $_POST["number"];
     $subject = $_POST["subject"];
     $message = $_POST["message"];
+    $numb = "Phone number: " . $number . "\n";
+    $text = $message .  "\n" . $numb;
     $headers = "From: " . $name . " <" . $email . ">\r\n";
-    $headers .= "Reply-To: " . $email . "\r\n"; // Add a Reply-To header
+    $headers .= "Reply-To: " . $email . "\r\n"; 
 
-    // Send email
-    if (mail($to, $subject, $message, $headers)) {
+ 
+    if (mail($to, $subject, $text, $headers)) {
         echo "OK";
     } else {
         echo "Email sending failed.";
-        // Additional error handling
-        //echo "Error: " . error_get_last()['message'];
+        
     }
-
 
 exit;
 
-    /*$to = "ayeshasheikh.as23@gmail.com";
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
-    
-    // Set additional headers
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-
-    // Send email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully.";
-    } else {
-        echo "Email sending failed.";
-        // Additional error handling
-        //echo "Error: " . error_get_last()['message'];
-    }
-exit;*/
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
-
-    require 'vendor/autoload.php';
-
-  //  $toEmail = "Technical.trainer@hcctechfoundation.com";
-      $toEmail = "khannnn839@gmail.com";
-    // $name = $_POST["name"];
-    // $email = $_POST["email"];
-    // $subject = $_POST["subject"];
-    // $message = $_POST["message"];
-    $name = 'HAMZA';
-    $email = "hafizumerkhan1@gmail.com";
-    $subject = 'Test';
-    $message ="Test email";
-    $mail = new PHPMailer(true);
-
-    try {
-        // $mail->isSMTP();
-        // $mail->Host = 'smtp.gmail.com';
-        // $mail->SMTPSecure = 'tls';
-        // $mail->Port = 587;
-        // $mail->SMTPAuth = true;
-        // $mail->Username = 'zainabnaveed.hcc@gmail.com';
-        // $mail->Password = 'Zain@2512';
-         $mail->isSMTP();
-        // $mail->Host       = 'smtp.meshmavens.com';
-        // $mail->Host       = 'localhost';
-        $mail->Host       = 'meshmavens.com';
-        $mail->SMTPSecure = 'tls'; 
-        $mail->SMTPAuth   = true;
-        //$mail->SMTPAuth   = false;
-       // $mail->Username   = 'info@meshmavens.com';
-       // $mail->Password   = 'Temp*4143';
-        $mail->Username   = 'khannnn839@gmail.com';
-        $mail->Password   = 'bywm mshj rran qhma';
-        $mail->Port = 587;
-
-        $mail->setFrom($email, $name);
-        $mail->addAddress($toEmail, 'Mesh team');
-
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->Body = $message;
-
-       $res =  $mail->send();
-       if($mail->send()){
-              echo "Your message has been sent. Thank you!";
-       }
-       echo $res;
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
 ?>
